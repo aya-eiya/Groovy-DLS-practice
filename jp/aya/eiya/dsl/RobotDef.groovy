55c468309e7fb2312f2af403027aa86a7ea88774
Hidetoshi Ayabe <lionehar@infoseek.jp>
2012-06-03 17:56:58 +0900

    Defining move method to the class.

--- jp/aya/eiya/dsl/RobotDef.groovy
+++ jp/aya/eiya/dsl/RobotDef.groovy
@@ -1,4 +1,12 @@
+enum Direction{
+  right,left,up,down
+}
+
 class Robot{
+  def move(final Direction direction){
+    println "$this moved to $direction"
+  }
 }
 
 def robot = new Robot()
+robot.move(Direction.left)
